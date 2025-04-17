709ad4d225dfb30f0cb9dc025ecbe0907f602fe4
gabrieldasneves <galvesdasneves@gmail.com>
2025-04-17 11:49:44 +0900

    feat:add README

--- front_mobile/src/app/index.tsx
+++ front_mobile/src/app/index.tsx
@@ -1,28 +1,9 @@
-import { colors } from "@/src/styles/theme";
-import { View, ImageBackground, StyleSheet, StatusBar } from "react-native";
-import { Initial } from "../components/organisms/pages/initial";
-import { Home } from "../components/organisms/pages/home";
+import { View, Text } from "react-native";
 
-export default function Index() {
+export default function App() {
   return (
-    <View style={{ flex: 1 }}>
-      <ImageBackground
-        source={require("@/src/assets/bones.jpeg")}
-        style={{ flex: 1 }}
-        resizeMode="cover"
-      >
-        <StatusBar translucent backgroundColor="transparent" />
-        <Initial />
-      </ImageBackground>
-      {/* <Home /> */}
+    <View>
+      <Text>Hello World</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  image: {
-    flex: 1,
-    width: "100%",
-    height: "100%",
-  },
-});
